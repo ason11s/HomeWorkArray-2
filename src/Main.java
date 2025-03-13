11b5da77939daa8671ead73a9ff90bc4ff8cf2d9
asonovserg <ason11s@bk.ru>
2025-03-13 22:05:20 +0500

    Correct

--- src/Main.java
+++ src/Main.java
@@ -35,23 +35,25 @@ public class Main {
         System.out.println("Task3");
 
         int [] payment = {5_000, 24_000, 11_000, 15_000, 19_000};
-        int total1 = 0;
+        double total1 = 0;
         double averageValue = 0;
         for(int i : payment){
             total1 = i + total1;
-            averageValue = total1 / 5;
         }
+
+        averageValue = total1 / payment.length;
         System.out.println("Средняя сумма трат за месяц составила " + averageValue + " рублей ");
 
         // Task 4
         System.out.println("Task4");
 
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0; i--) {
-            System.out.print(reverseFullName[i]);
-
-
+        for (int i = 0; i < reverseFullName.length / 2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - 1 - i];
+            reverseFullName[reverseFullName.length - 1 - i] = temp;
         }
+        System.out.println(reverseFullName);
 
     }
 
